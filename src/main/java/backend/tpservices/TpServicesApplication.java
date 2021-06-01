c77bb5627e45fa523e32d2abac6d9d41b7664beb
macak.nicolas@gmail.com <xmacakn@stuba.sk>
2021-06-01 08:48:33 +0200

    init

---
+++ src/main/java/backend/tpservices/TpServicesApplication.java
@@ -0,0 +1,13 @@
+package backend.tpservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backend/tpservices/TpServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.tpservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
