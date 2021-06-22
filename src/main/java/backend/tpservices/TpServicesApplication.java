d35a85aa28472d9dc4378a35eca893d621a5d896
macak.nicolas@gmail.com <xmacakn@stuba.sk>
2021-06-22 10:49:22 +0200

    conflict resolution

--- src/main/java/backend/tpservices/TpServicesApplication.java
+++ src/main/java/backend/tpservices/TpServicesApplication.java
@@ -9,6 +9,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 public class TpServicesApplication {
@@ -17,6 +21,20 @@ public class TpServicesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TpServicesApplication.class, args);
+	}
+
+	@EnableWebSecurity
+	@Configuration
+	class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable();
+					/*.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+					.authorizeRequests()
+					.antMatchers(HttpMethod.POST, "/login").permitAll()
+					.anyRequest().authenticated();*/
+		}
 	}
 
 	@Bean
