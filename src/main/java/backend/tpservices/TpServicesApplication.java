f2a242410cde02ea181814c3ff4c58ed0779bc55
macak.nicolas@gmail.com <xmacakn@stuba.sk>
2021-06-22 10:38:53 +0200

    csrf token. ešte sa neobnovuje

--- src/main/java/backend/tpservices/TpServicesApplication.java
+++ src/main/java/backend/tpservices/TpServicesApplication.java
@@ -4,12 +4,20 @@ import backend.tpservices.Models.Embedded.Contact;
 import backend.tpservices.Models.UserTypes.Client;
 import backend.tpservices.Repositories.ClientRepository;
 import backend.tpservices.Repositories.ContactRepository;
+import backend.tpservices.Security.JWTAuthorizationFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 
 @SpringBootApplication
 public class TpServicesApplication {
@@ -18,6 +26,20 @@ public class TpServicesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TpServicesApplication.class, args);
+	}
+
+	@EnableWebSecurity
+	@Configuration
+	class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable();
+					/*.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+					.authorizeRequests()
+					.antMatchers(HttpMethod.POST, "/login").permitAll()
+					.anyRequest().authenticated();*/
+		}
 	}
 
 	@Bean
